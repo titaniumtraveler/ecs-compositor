56bcd27515a458b09c5d7074ec1f3b25f0593ca9
Jon Heinritz <jon.heinritz@protonmail.com>
2025-09-14 20:55:17 +0200

    feat(example): update wayland-raw to new `core` and `codegen`

--- examples/wayland-raw/src/main.rs
+++ examples/wayland-raw/src/main.rs
@@ -1,14 +1,9 @@
-use crate::protocols::wayland::{
-    self,
-    wl_registry::{
-        self,
-        events::{Global, GlobalRemove},
-    },
-};
+use crate::protocols::wayland::{self, wl_display, wl_registry};
 use bstr::ByteSlice;
 use ecs_compositor_core::{
-    Message, NewId,
-    primitives::{Object, Primitive, Result, ThickPtr, UInt},
+    Opcode, RawSliceExt, enumeration, message_header, new_id, object,
+    primitives::{Result, Value},
+    uint,
 };
 use libc::{__errno_location, AF_UNIX, SOCK_STREAM, close, connect, sockaddr, sockaddr_un, socket};
 use std::{
@@ -19,10 +14,12 @@ use std::{
     num::NonZero,
     os::unix::{ffi::OsStrExt, prelude::RawFd},
     path::PathBuf,
-    ptr::{self, NonNull},
+    ptr::{self, slice_from_raw_parts, slice_from_raw_parts_mut},
 };
 
 mod protocols {
+    use ecs_compositor_core as proto;
+
     mod interfaces {
         pub use super::wayland::*;
     }
@@ -86,122 +83,143 @@ fn main() {
         let buf = {
             const BUF_SIZE: usize = 0x1000;
             let data = alloc::alloc(Layout::from_size_align_unchecked(BUF_SIZE, 4));
-            let Some(data) = NonNull::new(ptr::slice_from_raw_parts_mut(data, BUF_SIZE)) else {
+            if data.is_null() {
                 // allocation error
                 return;
-            };
-            data
+            }
+
+            slice_from_raw_parts_mut(data, BUF_SIZE)
         };
 
-        let mut cursor = ThickPtr {
-            ptr: buf.cast(),
-            len: buf.len(),
-        };
+        // make a copy
+        let mut cursor = buf;
+        let mut fds = slice_from_raw_parts_mut(ptr::null_mut(), 0);
 
-        let mut fds = ThickPtr {
-            // We currently don't support reading/writing FDs
-            ptr: NonNull::dangling(),
-            len: 0,
-        };
-
+        const WL_DISPLAY: u32 = 1;
         const WL_REGISTRY: u32 = 2;
-        let bind = wayland::wl_display::requests::GetRegistry {
-            registry: NewId {
+        let bind = wayland::wl_display::request::get_registry {
+            registry: new_id {
                 id: NonZero::new_unchecked(WL_REGISTRY),
                 _marker: PhantomData,
             },
         };
 
-        let header = WaylandHeader {
-            object_id: Object::from_id(NonZero::new_unchecked(1)),
+        let header = message_header {
+            object_id: object::from_id(NonZero::new_unchecked(1)),
             opcode: 1, // `wl_display::get_registry` is opcode `1`
-            datalen: (8 + bind.write_len()) as u16,
+            datalen: (8 + bind.len()) as u16,
         };
 
         header.write(&mut cursor, &mut fds).ok().unwrap();
         bind.write(&mut cursor, &mut fds).ok().unwrap();
 
-        // set cursor to the range that we just wrote to
-        cursor.len = cursor.ptr.byte_offset_from_unsigned(buf);
-        cursor.ptr = buf.cast();
-
+        cursor = slice_from_raw_parts_mut(
+            buf.cast(),
+            cursor.cast::<u8>().offset_from_unsigned(buf.cast()),
+        );
         loop {
-            let res = dbg!(libc::write(sock, cursor.ptr.as_ptr().cast(), cursor.len));
+            let res = dbg!(libc::write(sock, cursor.cast(), cursor.len()));
             if res < 0 {
                 panic!("io error: {}", *__errno_location());
             }
-            cursor.advance(res as usize);
+            cursor.split_at(res as usize).unwrap();
 
-            if cursor.len == 0 {
+            if cursor.len() == 0 {
                 break;
             }
         }
 
         // reset cursor
-        cursor.ptr = buf.cast();
-        cursor.len = buf.len();
+        cursor = buf;
 
         let mut data = {
-            let data = cursor.ptr;
+            let data = cursor;
             let mut len = 0;
             loop {
-                let res = libc::read(sock, cursor.ptr.as_ptr() as *mut _, cursor.len);
+                let res = libc::read(sock, cursor.cast(), cursor.len());
                 if res < 0 {
                     panic!("io error: {}", *__errno_location());
                 }
-                cursor.advance(res as usize);
+                cursor.split_at(res as usize).unwrap();
                 len += res as usize;
-                if WaylandHeader::write_len() as usize <= len {
+                if 8 <= len {
                     break;
                 }
             }
-            &*ptr::slice_from_raw_parts(data.as_ptr(), len)
+            slice_from_raw_parts(data.cast(), len)
         };
 
-        let mut fds: &[RawFd] = &[];
-        let Ok(header) = WaylandHeader::read(&mut data, &mut fds) else {
+        let fds: &mut *const [RawFd] = &mut (&[] as *const [RawFd]);
+        let Ok(header) = message_header::read(&mut data, &mut (&[] as *const _)) else {
             panic!("failed reading header");
         };
 
         println!("header: {header:#?}");
-
-        let true = (header.object_id.id().get() == 2 && [0, 1].contains(&header.opcode)) else {
-            panic!("invalid event")
-        };
 
         if data.len() < header.datalen as usize - 8 {
             panic!("not enough data on first read");
         }
 
         match (header.object_id.id().get(), header.opcode) {
-            (WL_REGISTRY, 0) => {
-                let Global {
-                    name,
-                    interface,
-                    version,
-                } = wl_registry::events::Global::read(data, &[]).ok().unwrap();
+            (WL_DISPLAY, code) if wl_display::event::Opcodes::from_u16(code).is_ok() => {
+                use wl_display::event as wl_display;
+                match wl_display::Opcodes::from_u16(code).unwrap() {
+                    wl_display::Opcodes::error => {
+                        let wl_display::error {
+                            object_id,
+                            code,
+                            message,
+                        } = wl_display::error::read(&mut data, fds).ok().unwrap();
 
-                let name = name.0;
-                let interface = {
-                    &*ptr::slice_from_raw_parts(
-                        interface.ptr.unwrap().as_ptr(),
-                        interface.len.get() as usize,
-                    )
+                        let object_id = object_id.id();
+                        let code = wayland::wl_display::enumeration::error::from_u32(code.0);
+                        let message = (&*slice_from_raw_parts(
+                            message.ptr.unwrap().as_ptr(),
+                            message.len.get() as usize,
+                        ))
+                            .as_bstr();
+
+                        println!(
+                            "wl_display::error(object_id: {object_id}, code: {code:?}, message: {message})"
+                        )
+                    }
+                    wl_display::Opcodes::delete_id => todo!(),
                 }
-                .as_bstr();
-                let version = version.0;
-
-                println!(
-                    "wl_registry::global(name: {name}, interface: {interface}, version: {version})"
-                );
             }
-            (WL_REGISTRY, 1) => {
-                let GlobalRemove { name } = wl_registry::events::GlobalRemove::read(data, &[])
-                    .ok()
-                    .unwrap();
-                let name = name.0;
+            (WL_REGISTRY, code) => {
+                use wl_registry::event as wl_registry;
+                match wl_registry::Opcodes::from_u16(code)
+                    .map_err(|i| format!("wl_registry: unknown opcode {i}"))
+                    .unwrap()
+                {
+                    wl_registry::Opcodes::global => {
+                        let wl_registry::global {
+                            name,
+                            interface,
+                            version,
+                        } = wl_registry::global::read(&mut data, fds).ok().unwrap();
 
-                println!("wl_registry::global_remove(name: {name})");
+                        let name = name.0;
+                        let interface = (&*slice_from_raw_parts(
+                            interface.ptr.unwrap().as_ptr(),
+                            interface.len.get() as usize,
+                        ))
+                            .as_bstr();
+                        let version = version.0;
+
+                        println!("wl_registry::global( n: {name} i: {interface} v: {version})");
+                    }
+                    wl_registry::Opcodes::global_remove => {
+                        let wl_registry::global_remove { name } =
+                            wl_registry::global_remove::read(&mut data, fds)
+                                .ok()
+                                .unwrap();
+
+                        let name = name.0;
+
+                        println!("wl_registry::global_remove(name: {name})");
+                    }
+                }
             }
             _ => panic!("unknown message"),
         }
@@ -212,34 +230,37 @@ fn main() {
 
 #[derive(Debug)]
 pub struct WaylandHeader {
-    pub object_id: Object,
+    pub object_id: object,
     pub datalen: u16,
     pub opcode: u16,
 }
 
-impl<'data> WaylandHeader {
-    fn read(data: &mut &'data [u8], fds: &mut &[RawFd]) -> Result<Self> {
-        let object_id = Object::read(data, fds)?;
-
-        let int = UInt::read(data, fds)?.0;
-
-        let datalen = (int >> 16) as u16;
-        let opcode = (int & 0xffff) as u16;
-
-        Ok(Self {
-            object_id,
-            datalen,
-            opcode,
-        })
-    }
-
-    fn write_len() -> u32 {
+impl Value<'_> for WaylandHeader {
+    fn len(&self) -> u32 {
         4 + 2 + 2
     }
 
-    fn write(&self, data: &mut ThickPtr<u8>, fds: &mut ThickPtr<RawFd>) -> Result<()> {
-        self.object_id.write(data, fds)?;
-        UInt((self.datalen as u32) << 16 | self.opcode as u32).write(data, fds)?;
-        Ok(())
+    unsafe fn read(data: &mut *const [u8], fds: &mut *const [RawFd]) -> Result<Self> {
+        unsafe {
+            let object_id = object::read(data, fds)?;
+            let i = uint::read(data, fds)?.0;
+
+            let datalen = (i >> 16) as u16;
+            let opcode = (i & 0xffff) as u16;
+
+            Ok(WaylandHeader {
+                object_id,
+                datalen,
+                opcode,
+            })
+        }
+    }
+
+    unsafe fn write(&self, data: &mut *mut [u8], fds: &mut *mut [RawFd]) -> Result<()> {
+        unsafe {
+            self.object_id.write(data, fds)?;
+            uint((self.datalen as u32) << 16 | self.opcode as u32).write(data, fds)?;
+            Ok(())
+        }
     }
 }
