33eae5611dab397c200dbab6ab4beb3feb6eef83
Jon Heinritz <jon.heinritz@protonmail.com>
2025-10-25 01:30:17 +0200

    fix(examples/wayland-raw): update build.rs

--- examples/wayland-raw/build.rs
+++ examples/wayland-raw/build.rs
@@ -1,7 +1,22 @@
+use std::path::PathBuf;
+
 fn main() {
-    ecs_compositor_codegen::protocol(
-        "../../wayland-protocols/wayland/protocol/wayland.xml",
-        "wayland-core.rs",
-        true,
-    );
+    let mut infile = PathBuf::new();
+    let mut outfile = PathBuf::new();
+
+    let outdir = &PathBuf::from(std::env::var_os("OUT_DIR").expect("missing outdir"));
+
+    println!("cargo::rerun-if-changed=build.rs");
+    {
+        infile.push("../../wayland-protocols/wayland/protocol/wayland.xml");
+        println!("cargo::rerun-if-changed={}", infile.display());
+
+        outfile.push(outdir);
+        outfile.push("wayland-core.rs");
+
+        ecs_compositor_codegen::protocol(&infile, &outfile, true);
+
+        infile.clear();
+        outfile.clear();
+    }
 }
